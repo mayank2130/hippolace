572717e5078e02c61b2e577ac096d69bbee85684
mayank2130 <mayankthakur1712@gmail.com>
2024-01-19 21:53:41 +0530

    authentication functionality

---
+++ src/trpc/auth-router.ts
@@ -0,0 +1,77 @@
+import { AuthCredentialsValidator } from "../lib/validators/account-credentials-validators";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { getPayloadClient } from "../get-payload";
+import { z } from 'zod'
+
+export const authRouter = router({
+  createpayloadUser: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input }) => {
+      const { email, password } = input;
+      const payload = await getPayloadClient();
+
+      const { docs: users } = await payload.find({
+        collection: "users",
+        where: {
+          email: {
+            equals: email,
+          },
+        },
+      })
+      if(users.length !==0) throw new TRPCError({code: "CONFLICT"})
+
+      await payload.create({
+        collection: "users",
+        data: {
+          email,
+          password,
+          role:"user"
+        },
+      })
+      return {success:true, sentToEmail: email}
+    }),
+
+    verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input
+
+      const payload = await getPayloadClient()
+
+      const isVerified = await payload.verifyEmail({
+        collection: 'users',
+        token,
+      })
+
+      if (!isVerified)
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+      return { success: true }
+    }),
+
+    
+    signIn: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input, ctx }) => {
+      const { email, password } = input
+      const { res } = ctx
+
+      const payload = await getPayloadClient()
+
+      try {
+        await payload.login({
+          collection: 'users',
+          data: {
+            email,
+            password,
+          },
+          res,
+        })
+
+        return { success: true }
+      } catch (err) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+      }
+    }),
+})
